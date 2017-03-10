6ebbd9cb6bea9f946823b7b3d9ccb9e4360adbfd
wuningjian <956980241@qq.com>
2017-03-10 17:53:50 +0800

    first commit

---
+++ assets/scripts/role.js
@@ -0,0 +1,34 @@
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        // foo: {
+        //    default: null,      // The default value will be used only when the component attaching
+        //                           to a node for the first time
+        //    url: cc.Texture2D,  // optional, default is typeof default
+        //    serializable: true, // optional, default is true
+        //    visible: true,      // optional, default is true
+        //    displayName: 'Foo', // optional
+        //    readonly: false,    // optional, default is false
+        // },
+        // ...
+        // 主角跳跃高度
+        jumpHeight: 0,
+        // 主角跳跃持续时间
+        jumpDuration: 0,
+        // 最大移动速度
+        maxMoveSpeed: 0,
+        // 加速度
+        accel: 0
+    },
+
+    // use this for initialization
+    onLoad: function () {
+
+    },
+
+    // called every frame, uncomment this function to activate update callback
+    // update: function (dt) {
+
+    // },
+});
